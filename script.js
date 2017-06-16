2c1d05e9485ee9d91c3dbed3573f642201059b40
Darya Vanchuk <dvanchuk@mail.ru>
2017-06-16 14:49:57 +0300

    add change text logics

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const testWrapper = document.querySelector(".test-wrapper");
 const testArea = document.querySelector("#test-area");
 const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
+const loadAnotherTextButton = document.querySelector("#load");
 const theTimer = document.querySelector(".timer");
+const anotherText = "This is an another text to test your typing skills. Enjoy!"
 
 var timer = [0,0,0,0];
-// Add leading zero to numbers 9 or below (purely for aesthetics):
 
 // Run a standard minute/second/hundredths timer:
 function runTimer(){
@@ -41,12 +42,19 @@ function reset(){
   timer[1]=0;
   timer[2]=0;
   timer[3]=0;
+  }
 }
 
+function loadText(){
+  if (document.querySelector("#origin-text p").innerHTML == originText) {
+    document.querySelector("#origin-text p").innerHTML = anotherText;
+  }else {
+      document.querySelector("#origin-text p").innerHTML = originText;
+  }
 }
 
 resetButton.addEventListener("click", reset, false);
-
+loadAnotherTextButton.addEventListener("click", loadText, false);
 // Event listeners for keyboard input and the reset button:
 testArea.addEventListener("keypress", start, false);
 testArea.addEventListener("keyup", spellCheck, false);
